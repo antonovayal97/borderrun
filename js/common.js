9d08018127f8a46d0ab94c97fbcf8a3333e2389a
Ayal Antonov <antonovayal97@gmail.com>
2025-02-04 11:01:04 +0700

    first commit

---
+++ js/common.js
@@ -0,0 +1,32 @@
+window.BACKEND_URL = "http://localhost/";
+
+document.addEventListener("DOMContentLoaded",(event) => {
+    const html = document.querySelector("html");
+    function initTelegramWebApp()
+    {
+        let telegramTest = document.querySelector("#telegramTest");
+        telegramTest.innerText = JSON.stringify(window.Telegram.WebApp.initData, null, 4);
+    }
+    function checkTheme()
+    {
+        var theme_controllers = document.querySelectorAll(".theme-controller");
+        if(window.Telegram.WebApp.colorScheme == "light")
+        {
+            html.dataset.theme = "light";
+            theme_controllers.forEach((controller) => {
+                controller.value = "dark";
+            })
+            return;
+        }
+        html.dataset.theme = "dark";
+        theme_controllers.forEach((controller) => {
+            controller.value = "light";
+        })
+    }
+    function init()
+    {
+        initTelegramWebApp();
+        checkTheme();
+    }
+    init();
+});
