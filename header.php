73cc990471a702eda45beb57531818b560570bd0
Ayal <antonovayal97@gmail.com>
2025-02-28 01:56:07 +0700

    fix

--- header.php
+++ header.php
@@ -10,11 +10,23 @@
         <title><? $APPLICATION->ShowTitle() ?></title>
         <!--<script src="https://unpkg.com/htmx.org"></script>-->
         <script src="https://telegram.org/js/telegram-web-app.js?56"></script>
+        <?
+        if($APPLICATION->GetCurDir() == "/"):
+        ?>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.2.3/flatpickr.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.2.3/flatpickr.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/ru.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.10.1/lottie.min.js"></script>
         <script src="https://yookassa.ru/checkout-widget/v1/checkout-widget.js"></script>
+        <?
+        endif;
+        ?>
+        <?
+        if($APPLICATION->GetCurDir() == "/success_page/"):
+        ?>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.10.1/lottie.min.js"></script>
+        <?
+        endif;
+        ?>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap" rel="stylesheet">
